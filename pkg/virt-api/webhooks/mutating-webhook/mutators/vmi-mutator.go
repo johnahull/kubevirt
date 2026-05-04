daa91780a58622bd4f8720124f6aea2b3503e0f8
John Hull <jhull@redhat.com>
2026-05-04 14:22:32 -0500

    Force root mode and add CAP_SYS_RESOURCE for VFIO VMIs
    VFIO passthrough fails in non-root mode (the default) because libvirt
    calls prlimit() on the QEMU child process to set RLIMIT_MEMLOCK, and
    the Linux kernel requires CAP_SYS_RESOURCE for prlimit() on another
    process regardless of whether the value is being raised or lowered.
    
    virt-handler's external prlimit64 on virtqemud works correctly, but
    libvirt's internal prlimit(QEMU_pid, RLIMIT_MEMLOCK, 2^53-1) from
    virProcessSetMaxMemLock() fails with EPERM in the unprivileged
    container.
    
    Skip markAsNonroot() for VFIO VMIs so the container runs as root,
    and add CAP_SYS_RESOURCE since it is not in the default root
    capability set.
    
    Non-VFIO VMs remain non-root with minimal capabilities.
    
    Fixes: https://github.com/kubevirt/kubevirt/issues/17694
    Related: https://github.com/kubevirt/kubevirt/issues/12433
    Related: https://github.com/kubevirt/kubevirt/issues/10379
    
    Signed-off-by: John Hull <jhull@redhat.com>

--- pkg/virt-api/webhooks/mutating-webhook/mutators/vmi-mutator.go
+++ pkg/virt-api/webhooks/mutating-webhook/mutators/vmi-mutator.go
@@ -83,7 +83,7 @@ func ApplyNewVMIMutations(newVMI *v1.VirtualMachineInstance, clusterConfig *virt
 		}
 	}
 
-	if !clusterConfig.RootEnabled() {
+	if !clusterConfig.RootEnabled() && !util.IsVFIOVMI(newVMI) {
 		markAsNonroot(newVMI)
 	}
 
